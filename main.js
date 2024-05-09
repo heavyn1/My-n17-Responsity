6b73179bab47b7cb85043daefee94dbd98060e7b
heavyn1 <bunedusupov968@gmail.com>
2024-05-09 14:57:01 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+let a = +prompt(" hohlagan son kiriting ..");
+let b = +prompt("hohlagan son kiriting ..");
+let c;
+
+
+
+// Decloration Function
+function DecloreFunc(a, b) {
+    if (c = Math.max(a, b)) {
+        alert(c);
+    } else  if (c = a === b) {
+        alert(a + " va " + b + " bir biriga teng")
+    } 
+}
+DecloreFunc(a, b);
+   
+    
